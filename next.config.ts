c64f2857b67f5e0422d522f06ed56869ecbc5e2e
lanstheprodigy <alan.040219.gam@gmail.com>
2025-10-17 01:35:49 +0700

    deployment: remove standalone config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  experimental: {},
-};
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
